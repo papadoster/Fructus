265896c25731f2ded0c5f8f1a405a71b473e66f4
Aleksandr Karpov <papadoster13@gmail.com>
2023-02-17 21:32:59 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Александр Карпов on 17.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
